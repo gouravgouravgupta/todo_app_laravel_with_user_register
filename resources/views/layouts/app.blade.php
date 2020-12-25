7f0658ad7e2d7d6b396dc29e36fca200cf710b5e
gouravgouravgupta <gouravgupta7879@gmail.com>
2020-12-25 09:53:01 +0530

    add meta tag for csrf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta  http-equiv="refresh" content="{{ config('session.lifetime')*60}}">
+        <title>{{ config('app.name', 'Todo APP') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
